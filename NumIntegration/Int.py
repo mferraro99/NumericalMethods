7bb33599c948267ab147a44b65a1822dc3456b29
Marcello Ferraro <143500201+mferraro99@users.noreply.github.com>
2023-10-01 12:40:49 +0200

    Update Int.py

--- NumIntegration/Int.py
+++ NumIntegration/Int.py
@@ -17,8 +17,8 @@ h = 10**-8 # Increment for derivatives
 # Boundaries
 a = 0
 b = pi
-# Number of points
-N = 128
+# Number of points -> 128 points? Then N=129 (and so on...)
+N = 129
 
 #### Functions
 
@@ -41,6 +41,12 @@ def d2f(x,h):
     denominator = h**2
     return numerator/denominator
 
+# Calculate the third derivative in a point using the central
+# difference method 
+def d3f(x,h):
+    numerator = f(x-2*h)-2*f(x-h)+2*f(x+h)-f(x+2*h)
+    denominator = 2*h**3
+    return numerator/denominator
 
 #####################################################################
 # *** Method 1: Trapezoidal rule (No error correction)***
@@ -56,7 +62,7 @@ fx = []
 for i in x: fx.append(f(i))
 
 d2f_trapez = []
-I,Err_asympt = 0,0,0
+I,Err_asympt = 0,0
 for i in range(0,len(fx)-1):
 
     # In integral
@@ -66,7 +72,6 @@ for i in range(0,len(fx)-1):
     # E_n asymptotic error
     Err_n = -1/12 * (x[i+1]-x[i])**2 * (df(x[i+1],h)-df(x[i],h))
     Err_asympt += Err_n
-
 
 #####################################################################
 # *** Method 2: Corrected trapezoidal rule ***
@@ -89,5 +94,25 @@ for i in range(len(fx)-1):
     I_n = ((x[i+1]-x[i])/2)/3 * (f(x[i+1])+4*f(1/2*(x[i+1]+x[i]))+f(x[i]))
     I += I_n
 
+print(I)
 #####################################################################
 # *** Method 4: Simpson's composite rule ***
+
+I = 0
+for i in range(len(fx)):
+
+    if i == 0 or i == len(fx)-1:
+        c = 1
+        print(i,c)
+    else:
+        if i % 2 != 0: # i is odd
+            c = 4
+            print(i,c)
+        else: # i is even
+            c = 2
+            print(i,c)
+    
+    I += c * f(x[i])
+
+I = (x[1]-x[0])/3 * I
+print(I)
